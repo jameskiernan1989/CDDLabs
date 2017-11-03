1f01019d718140f79c7e43c77cffbfb9503815b6
James Kiernan <jameskiernan1989@hotmail.com>
2017-11-03 12:20:39 +0000

    Updated readme file

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -10,30 +10,7 @@
 #include <iostream>
 #include <thread>
 
-std::shared_ptr<Semaphore> barrier(new Semaphore(0)); /*Global instance of a barrier semaphore*/
-int count = 0; /*Holds the count of the number of threads*/
-int n = 4; /*The number of threads */
-
-/*! \fn void task(std::shared_ptr<Semaphore> mutex)
-  \brief This function will be called by each thread.
-  \param mutex A semaphore instance used for a  mutual exclusion lock
- */
-void task(std::shared_ptr<Semaphore> mutex){
-  std::cout<<"Current value of count:"<< count << std::endl;
-  mutex.wait();
-  count = count + 1;
-  std::cout<<"Count is now:" << count << std:endl;
-  mutex.signal();
-
-  if(count == n){
-    std::cout<<"All threads entered. Unlocking barrier..."<<std::endl;
-    barrier.signal();
-  }
-
-  barrier.wait();
-  barrier.signal();
-}
-
+int task_counter = 0;
  /*! \fn int main()
     \brief Keeps track of how many threads arrive. Mutex provides exclusive access to count so that threads can increment it safely.
 Barrier is locked until all threads arrive, then it unlocks.
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CDDLabs
 4th year lab exercises for Concurrent and Distributed Development module.
+Lab One:
+Program to demonstrate two threads signalling each other using Semaphores.
+Lab Two:
+Program to demonstrate the Rendezvous using Semaphores.
+Lab Three:
+Program to demonstrate Mutual Exclusion using Semaphores.
+Lab Four:
+Program to demonstrate the Reusable Barrier class using Semaphores.
