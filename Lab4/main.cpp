2ba6b3c58d838bc97de28ba2b371cd05c393c384
James Kiernan <jameskiernan1989@hotmail.com>
2017-11-08 11:59:52 +0000

    main.cpp file updated

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -6,26 +6,14 @@
 !
 */
 
-#include "Semaphore.h"
+#include "Barrier.h"
 #include <iostream>
 #include <thread>
 
-int task_counter = 0;
  /*! \fn int main()
-    \brief Keeps track of how many threads arrive. Mutex provides exclusive access to count so that threads can increment it safely.
-Barrier is locked until all threads arrive, then it unlocks.
+    \brief Runs the program
 */
 int main(void){
-  std::thread threadOne, threadTwo, threadThree, threadFour;
-  std::shared_ptr<Semaphore> mutex(new Semaphore(1));
-  threadOne=std::thread(task, mutex);
-  threadTwo=std::thread(task, mutex);
-  threadThree=std::thread(task, mutex);
-  threadFour=std::thread(task, mutex);
-  std::cout<<"Started from main"<<std::endl;
-  threadOne.join();
-  threadTwo.join();
-  threadThree.join();
-  threadFour.join();
+  
   return 0;
 }
