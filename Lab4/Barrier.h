34c26da151b7e38681176baa45a2807d4c447491
James Kiernan <jameskiernan1989@hotmail.com>
2017-11-08 11:51:29 +0000

    Updates to Barrier.h and Barrier.cpp files

--- Lab4/Barrier.h
+++ Lab4/Barrier.h
@@ -1,5 +1,5 @@
 /*!\author James Kiernan
-    \date 25/10/2017
+    \date 8/11/2017
     \file Barrier.h
     \brief The Barrier class
 	\GNU General Public Licence
@@ -8,18 +8,41 @@
 */
 #include "Semaphore.h"
 #include <iosteam>
-#include <thread>
+#include <stdio.h>
 /*! \class Barrier
     \brief Class Barrier is used to cause threads to wait until they each perform a certain function or each reach a particular point in their execution.
 */
 
-class barrier
-{
+class barrier{
+  
+ private:
+  int threadCount; // Number of threads using the barrier
+  int threadNum; // current thread
+  int gate; // current gate active
+  std::shared_ptr<Semaphore> mutex; //mutex to control thread execution
+  std::shared_ptr<Semaphore> barrierA; // first gate 
+  std::shared_ptr<Semaphore> barrierB; // second gate
+
+  void gateA(); // first gate of the barrier
+  void gateB(); // second gate of the barrier
+  
  public:
-  barrier(std::shared_ptr<Semaphore> sem, unsigned int t_count=0);
-  ~barrier();
+  
+  /*!
+\func Barrier()
+\param int threadCount - The amount of threads that will be using the barrier
+\brief Barrier class constructor. This lets the Barrier know how many threads will be passing through it, and initializes the current thread number, which gate is being currently used, semaphore instances of a Mutual Exclusion lock and two barriers to control thread flow.
 
-  bool wait();
-  bool signal();
+ */
+  Barrier(int threadCount);
+  
+  /*!
+\func ~Barrier
+\param none
+\brief Barrier class destructor.
+ */
+  ~Barrier();
+  void setThreadCount(int threadCount);
+  int getThreadCount();
+  void waitForThreads();
 };
-
